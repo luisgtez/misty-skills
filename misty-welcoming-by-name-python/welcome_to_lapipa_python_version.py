7f123af64fcce381487a3a05d3c771e9693ca5a5
Manolo Rosario Evans Diaz <dragon.evans@gmail.com>
2022-07-19 16:46:44 +0200

    Frame for converting ALL functions to python from JS.

--- misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
+++ misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
@@ -6,4 +6,22 @@ misty.start_face_recognition()
 
 
 def registerFaceDetection ():
-# Enter Register Face Detection here in python.
+    # Create Register Face Detection here in python.
+
+def _FaceDetect(data):
+    # Create _FaceDetect here in python.
+
+def _TimeoutToNormal():
+    # Create _timeoutToNormal here in python.
+
+def _VoiceRecord():
+    # Create Voice record function here in python.
+
+def _GetAudioFile():
+    # Create GetAudioFile here in python.
+
+def _SendExternalRequest():
+    # Create SendExternalRequest here in python.
+
+
+
