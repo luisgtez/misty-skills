b4f5ab2a801ec2b017d1a8d825fac64ae559d81a
luisgtez <luis.gutierrez@bedrockdbd.com~>
2022-07-22 10:43:28 +0200

    Added audio functions to record and play audio

--- misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
+++ misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
@@ -1,9 +1,54 @@
 from mistyPy.Robot import Robot
 from mistyPy.Events import Events
-from mistyPy.EventFilters import EventFilters
+import json
 
-misty.start_face_recognition()
+###############################################AUDIO###################################################################
 
+def AudioFun():
+    misty.RegisterEvent("Introduction", Events.TextToSpeechComplete, keep_alive=False, callback_function=CaptureSpeech)
+    misty.Speak("Talk", utteranceId= 'tts-content')
+    
+    
+def CaptureSpeech(event):
+    misty.RegisterEvent("StartRecord", Events.VoiceRecord, debounce=10000,callback_function=PlayAudio)
+    misty.CaptureSpeech(silenceTimeout=1000)
+    
+def PlayAudio(event):
+    file_name = event['message']['filename']
+    print(file_name)
+    misty.PlayAudio(file_name, volume= 100)
 
-def registerFaceDetection ():
-    misty.speak('Test')
+def DownloadAudio(event):
+    file_name = event['message']['filename']
+    print(file_name)
+    misty.GetAudioFile(file_name)
+    
+###############################################FACE#########################################################
+    
+def FaceFun():
+    misty.RegisterEvent(event_name='FaceRecog',event_type=Events.FaceRecognition,callback_function=FaceRecog)
+    misty.StartFaceDetection()
+    misty.StartFaceRecognition()
+    
+def FaceRecog(event):
+    print(event)
+    misty.StopFaceRecognition()
+    misty.StopFaceDetection
+    
+
+###############################################SCRIPTS#########################################################
+
+def GetAudioList():
+    x = json.loads(misty.GetAudioList().text)['result']
+    l = []
+    for i in range(len(x)):
+        if x[i]['systemAsset'] == False:
+            l.append(x[i]['name'])
+    return l
+
+###############################################TEST-CODE#########################################################
+
+ip_address = '192.168.128.86'
+misty = Robot(ip_address)
+
+AudioFun()
