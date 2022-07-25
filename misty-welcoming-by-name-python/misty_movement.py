55fe70fbdfece6f6cbd092192d2a494da2c067a1
luisgtez <luis.gutierrez@bedrockdbd.com~>
2022-07-25 11:05:41 +0200

    Implemented welcome_to_lapipa face recognition

--- misty-welcoming-by-name-python/misty_movement.py
+++ misty-welcoming-by-name-python/misty_movement.py
@@ -1,27 +1,40 @@
-from shutil import move
 from mistyPy.Robot import Robot
 from mistyPy.Events import Events   
 from curtsies import Input
 
 def movement():
     with Input(keynames='curses') as input_generator:
+        agVel = 0
+        linVel = 0
+        misty.MoveHead(0,0,0)
         for e in input_generator:
-            print(e)
-            misty.MoveHead(0,0,0)
             if e == 'w':
-                misty.Drive(50,0)
+                agVel =0
+                linVel = speed
+            if e == 'a':
+                agVel = speed
+                linVel = 0
             if e == 's':
-                misty.Drive(q0,0)
-            if e == 'e':
-                misty.Drive(0,-100)
+                agVel = 0
+                linVel = -speed
+            if e == 'd':
+                agVel = -speed
+                linVel =0
             if e == 'q':
-                misty.Drive(0,100)
-            if e=='KEY_SPACE':
-                misty.Drive(0,0)
-                
+                agVel = speed
+                linVel = speed
+            if e == 'e':
+                agVel = -speed
+                linVel = speed
+            if e == 'c':
+                agVel = 0
+                linVel = 0 
+            
+            misty.Drive(angularVelocity=agVel,linearVelocity=linVel)
 
 
 ip_address = '192.168.128.86'
 misty = Robot(ip_address)
+speed = input('Velocidad: ')
 
 movement()
--- misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
+++ misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
@@ -1,43 +1,99 @@
 from mistyPy.Robot import Robot
 from mistyPy.Events import Events
 import json
+from time import time
+
+
+def Start_Face_Recognition():
+    print('\nSTART FACE RECOGNITION FUNCTION')
+    misty.RegisterEvent("Face", Events.FaceRecognition, callback_function=FaceDetect)
+    misty.StartFaceDetection()
+    misty.StartFaceRecognition()
+
+
+def FaceDetect(event):
+    hora = time()
+    print('\nSTART FACE DETECT FUNCTION')
+    misty.StopFaceRecognition()
+    misty.StopFaceDetection()
+    Name = event['message']['personName']
+    if Name == 'unknown person':
+        misty.ChangeLED(148, 0, 211)
+        misty.DisplayImage("e_Contempt.jpg")
+        misty.Speak("Hello human, welcome to La Pipa! What's your name?")
+        CaptureSpeech()
+        Wait(hora,7)
+        ReturnToNormal()
+    else:
+        misty.ChangeLED(0, 0, 111)
+        misty.DisplayImage("e_Joy2.jpg")
+        string = 'Hi ' + Name + ' we have met before'
+        misty.Speak(string)
+        misty.MoveArm("both", -26, 100)
+        misty.Stop(1000)
+        misty.MoveArm("both", 90, 100)
+        Wait(hora,7)
+        hora = time()
+        ReturnToNormal()
+        
+                
 
 ###############################################AUDIO###################################################################
 
-def AudioFunc():
-    misty.RegisterEvent("Introduction", Events.TextToSpeechComplete, keep_alive=False, callback_function=CaptureSpeech)
-    misty.Speak("Hi", utteranceId= 'tts-content')
-    
-    
-def CaptureSpeech(event):
-    misty.RegisterEvent("StartRecord", Events.VoiceRecord, debounce=10000,callback_function=GetFileName)
-    misty.CaptureSpeech(silenceTimeout=1000)
-    
+
+def CaptureSpeech():
+    print('\nSTART CAPTURE SPEECH FUNCTION')
+    misty.ChangeLED(0, 0, 255)
+    misty.RegisterEvent("StartRecord", Events.VoiceRecord,
+                        debounce=10000, callback_function=GetFileName)
+    misty.CaptureSpeech(silenceTimeout=1000, maxSpeechLength=5000)
+
+
 def GetFileName(event):
+    print('\nSTART GET FILE NAME FUNCTION')
     file_name = event['message']['filename']
+    data = misty.GetAudioFile(file_name, base64=True)
+    _GetAudioFile(data)
+
+##############################################GOOGLE API#########################################################
 
 
-# DownloadAudio doesnt currently work
-#def DownloadAudio():
-    #file_name = event['message']['filename']
-    # url = 192.168.128.86/api/audio?FileName=test3_luis.wav
-    #file_name = 'test3_luis.wav'
-    #misty.GetAudioFile(file_name, 'ProcessAudioFile')
-    
-###############################################FACE#########################################################
-    
-def FaceFunc():
-    misty.RegisterEvent(event_name='FaceRecog',event_type=Events.FaceRecognition,callback_function=FaceRecog)
-    misty.StartFaceDetection()
-    misty.StartFaceRecognition()
-    
-def FaceRecog(event):
-    print(event)
-    misty.StopFaceRecognition()
-    misty.StopFaceDetection
-    
+def _GetAudioFile(data):
+    print('\nSTART GET AUDIO FILE FUNCTION')
+    base64 = data.json()['result']['base64']
+    respuest = misty.SendExternalRequest(
+        "POST",
+        "https://speech.googleapis.com/v1p1beta1/speech:recognize?key=" + apikey,
+        None,
+        None,
+        json.dumps({"audio": {'content': base64}, "config": {"enableAutomaticPunctuation": True,
+                   "encoding": "LINEAR16", "languageCode": "es-ES", "model": "command_and_search"}})
+    )
 
-###############################################SCRIPTS#########################################################
+    _SendExternalRequest(respuest)
+
+
+def _SendExternalRequest(data):
+    print('\nSTART SEND EXTERNAL REQUEST FUNCTION')
+    respuesta = data.json()['results'][0]['alternatives'][0]['transcript']
+    print('Misty heard: ' + respuesta)
+    if not respuesta:
+        misty.Speak("I'm sorry I couldn't hear you well.")
+    else:
+        misty.Speak("Please stand still for a second")
+        misty.StartFaceTraining(respuesta)
+
+##############################################SCRIPTS#########################################################
+
+def Wait(hora,seconds):
+    vuelve =True
+    while vuelve:
+        if time()-hora > seconds:
+            vuelve=False
+
+def ReturnToNormal():
+    misty.DisplayImage('e_DefaultContent.jpg')
+    misty.ChangeLED(0, 0, 0)
 
 def GetAudioList():
     x = json.loads(misty.GetAudioList().text)['result']
@@ -49,5 +105,13 @@ def GetAudioList():
 
 ###############################################TEST-CODE#########################################################
 
+
 ip_address = '192.168.128.86'
 misty = Robot(ip_address)
+apikey = 'YOUR APIKEY'
+
+misty.MoveHead(-5, 0, 0)
+misty.DisplayImage('e_DefaultContent.jpg')
+misty.ChangeLED(0, 0, 0)
+
+Start_Face_Recognition()
